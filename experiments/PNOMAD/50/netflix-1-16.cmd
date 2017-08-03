ad7afa10bb457894aa9a6819f48e3d8f9a7dee60
Hui Li <hli2@cs.hku.hk>
2017-08-03 21:12:14 +0200

    add experiment scripts

---
+++ experiments/PNOMAD/50/netflix-1-16.cmd
@@ -0,0 +1,82 @@
+#!/bin/sh
+###############################################################################
+###  This is a sample PBS job script for Serial C/F90 program                 #
+###  1. To use GNU Compilers (Default)					      #
+###     gcc hello.c -o hello-gcc					      #
+###     gfortran hello.f90 -o hello-gfortran				      #
+###  2. To use PGI Compilers						      #
+###     module load pgi							      #
+###     pgcc hello.c -o hello-pgcc					      #
+###     pgf90 hello.f90 -o hello-pgf90					      #
+###  3. To use Intel Compilers						      #
+###     module load intel					              #
+###     icc hello.c -o hello-icc        				      #
+###     ifort hello.f90 -o hello-ifort					      #
+###############################################################################
+
+### Job name
+#PBS -N NOMAD-n-50-1-16
+### Declare job non-rerunable
+#PBS -r n
+#PBS -k oe
+
+###  Queue name (debug, parallel or fourday)   ################################
+###    Queue debug   : Walltime can be  00:00:01 to 00:30:00                  # 
+###    Queue parallel: Walltime can be  00:00:01 to 24:00:00                  #
+###    Queue fourday : Walltime can be  24:00:01 to 96:00:00                  #
+###  #PBS -q parallel                                                         #
+###############################################################################
+#PBS -q parallel
+
+###  Wall time required. This example is 30 min  ##############################
+###  #PBS -l walltime=00:30:00                   			      #
+###############################################################################
+#PBS -l walltime=24:00:00
+
+###  Number of node and cpu core  ############################################# 
+###  For serial program, 1 core is used					      #
+###  #PBS -l nodes=1:ppn=1						      #
+###############################################################################
+#PBS -l nodes=1:ppn=16
+
+###############################################################################
+#The following stuff will be executed in the first allocated node.            #
+#Please don't modify it                                                       #
+#                                                                             #
+echo $PBS_JOBID : `wc -l < $PBS_NODEFILE` CPUs allocated: `cat $PBS_NODEFILE`
+PATH=$PBS_O_PATH
+JID=`echo ${PBS_JOBID}| sed "s/.hpc2015-mgt.hku.hk//"`
+###############################################################################
+
+echo ===========================================================
+echo "Job Start  Time is `date "+%Y/%m/%d -- %H:%M:%S"`"
+
+cd $PBS_O_WORKDIR
+
+######################################
+
+cd ../../../build/
+
+data_folder="/data/huilee/mf_data/netflix"
+
+default_timeouts="10 50 100 150 200 300 500 1000"
+dimension="50"
+lambda="0.05"
+l="0.002"
+thread="16"
+
+OUTFILE="log-netflix-50-1-16.txt"
+
+for timout in $default_timeouts; do
+	time ./nomad_double --nthreads $thread --lrate $l --reg $lambda --dim $dimension --path $data_folder --timeout $timout >> ${OUTFILE}
+done
+
+mv $OUTFILE $log_file $PBS_O_WORKDIR
+
+######################################
+
+echo "Job Finish Time is `date "+%Y/%m/%d -- %H:%M:%S"`"
+mv $HOME/${PBS_JOBNAME}.e${JID} $HOME/${PBS_JOBNAME}.o${JID} $PBS_O_WORKDIR
+
+exit 0
+
